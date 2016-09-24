3c72bfbec2cd89a009b365711a8a7c8d588ed523
elrincondejose25 <joseayala.msn2016@gmail.com>
2016-09-24 11:22:27 -0300

    Add files via upload

---
+++ EXAMEN 1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int;
+    char titulo[50];
+    int anio;
+    char nacionalidad[50];
+    int director;
+}
+typedef struct
+{
+    int id_pelicula;
+    char titulo[50];
+    int anio;
+    char nacionalidad[50];
+    int id_director;
+}
+typedef struct
+{
+    int id_director;
+    char nombre[50];
+    char nacionalidad[50];
+    ;
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
